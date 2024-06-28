df06c6e8dcd856bf166cc118ef7a132ce97c359b
adriantaf <atafoya863@gmail.com>
2024-06-27 19:54:44 -0700

    Actualizacion informacion

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const URL_BASE = 'https://adriantaf.github.io/';
+// const URL_BASE = 'https://adriantaf.github.io/';
+const URL_BASE = 'http://localhost:8080/';
 let jsonFileProyectos = URL_BASE + "src/json/proyectos.json";
 
 const MAIN = document.getElementById('main');
@@ -19,53 +20,55 @@ function createItems(data) {
 
 		fragment.getElementById('nombre').innerText = element.nombre;
 		fragment.getElementById('descripcion').innerText = element.descripcion;
-		fragment.getElementById('version').innerText = parseFloat(element.version);
-		fragment.getElementById('estado').innerText = element.estado ? "Completado" : "En proceso";
 
-		// TECNOLOGIAS
-		element.tecnologias.forEach((tecnologia, i, arr) => {
-			if (tecnologia === 'js') {
-				arr[i] = 'JavaScript';
-			} else if (tecnologia === 'mysql') {
-				arr[i] = 'MySQL';
-			} else {
-				arr[i] = tecnologia.toUpperCase();
-			}
-		});
-		tecnologiasString = element.tecnologias.join(', ');
-		fragment.getElementById('tecnologias').innerText = tecnologiasString;
+		if (element.is_top) {
+			// TECNOLOGIAS
+			element.tecnologias.forEach((tecnologia, i, arr) => {
+				if (tecnologia === 'js') {
+					arr[i] = 'JavaScript';
+				} else if (tecnologia === 'mysql') {
+					arr[i] = 'MySQL';
+				} else {
+					arr[i] = tecnologia.toUpperCase();
+				}
+			});
 
-		// LINK
-		if (element.preview) {
-			fragment.getElementById('link').href = `https://adriantaf.github.io/${element.id}`;
-		} else {
-			fragment.getElementById('link').parentNode.removeChild(fragment.getElementById('link'));
-		}
-		// ICONO TOP
-		if (!element.is_top) {
+			tecnologiasString = element.tecnologias.join(', ');
+			fragment.getElementById('tecnologias').innerText = tecnologiasString;
 			fragment.getElementById('top-icon').parentNode.removeChild(fragment.getElementById('top-icon'));
-			// CONTAINER ACTIONS
-			if (!element.preview) {
+			// LINK
+			if (element.url !== "") {
+				fragment.getElementById('link').href = element.url;
+			} else {
+				// fragment.getElementById('link').parentNode.removeChild(fragment.getElementById('link'));
 				fragment.getElementById('cont-actions').parentNode.removeChild(fragment.getElementById('cont-actions'));
 			}
-		}
-		// SCROLL ICON
-		if (element.src.length < 2) {
-			fragment.getElementById('scroll-icon').parentNode.removeChild(fragment.getElementById('scroll-icon'));
-		}
-		// SLIDER
-		element.src.forEach(srcImage => {
-			let image = document.createElement('img');
-			image.src = `./res/img-proyectos/${srcImage}`;
-			image.classList.add('card__slider__img');
-			fragmentImages.appendChild(image);
-		});		
+			// // ICONO TOP
+			// if (element.is_top) {
+			// 	fragment.getElementById('top-icon').parentNode.removeChild(fragment.getElementById('top-icon'));
+			// 	// CONTAINER ACTIONS
+			// 	if (element.url === "") {
+			// 		fragment.getElementById('cont-actions').parentNode.removeChild(fragment.getElementById('cont-actions'));
+			// 	}
+			// }
+			// SCROLL ICON
+			if (element.src.length < 2) {
+				fragment.getElementById('scroll-icon').parentNode.removeChild(fragment.getElementById('scroll-icon'));
+			}
+			// SLIDER
+			element.src.forEach(srcImage => {
+				let image = document.createElement('img');
+				image.src = `./res/img-proyectos/${srcImage}`;
+				image.classList.add('card__slider__img');
+				fragmentImages.appendChild(image);
+			});		
 
-		fragment.getElementById('slider').append(fragmentImages);
-		element.is_top ? elementosOrdenados.unshift(fragment) : elementosOrdenados.push(fragment);
+			fragment.getElementById('slider').append(fragmentImages);
+			element.is_top ? elementosOrdenados.unshift(fragment) : elementosOrdenados.push(fragment);
+		}
 	}
 
-	elementosOrdenados.forEach(item => MAIN.appendChild(item));
+	elementosOrdenados.forEach(item => MAIN.prepend(item));
 }
 
 fetch(jsonFileProyectos)
