7ce0099655ef43f0725211e25d484c3abf12c09a
adriantaf <atafoya863@gmail.com>
2024-12-29 12:03:34 -0800

    Modo claro con video

--- src/scripts/toggle_theme.js
+++ src/scripts/toggle_theme.js
@@ -2,22 +2,30 @@
 const btnTheme = document.getElementById('btn-theme');
 const txtBtnTheme = document.getElementById('txt-theme');
 const html = document.documentElement;
+const THEME = {
+  dark: "dark",
+  light: "light"
+}
+const STR_THEME = {
+  dark: "oscuro",
+  light: "claro"
+}
 
 function toggleTheme() {
-  const isDark = html.classList.toggle('dark');
-  txtBtnTheme.innerText = `Tema ${isDark ? 'claro' : 'oscuro'}`;
-  localStorage.setItem('theme', isDark ? 'dark' : 'light');
+  const themeIsDark = html.getAttribute('data-theme') === THEME.dark;
+  html.setAttribute('data-theme', themeIsDark ? THEME.light : THEME.dark);
+  localStorage.setItem('theme', themeIsDark ? THEME.light : THEME.dark);
+  // Modificar el botón
+  txtBtnTheme.innerText = `Tema ${themeIsDark ? STR_THEME.light : STR_THEME.dark}`;
+  document.body.style.transition = "background .6s";
+    document.getElementById('main-container').style.transition = "background .6s";
 }
 
 // Inicializar el tema desde localStorage
-const savedTheme = localStorage.getItem('theme') || 'light';
-if (savedTheme === 'dark') {
-  html.classList.add('dark');
-  txtBtnTheme.innerText = `Tema claro`;
-} else {
-  html.classList.remove('dark');
-  txtBtnTheme.innerText = `Tema oscuro`;
-}
+const savedTheme = localStorage.getItem('theme') || THEME.dark;
+html.setAttribute('data-theme', savedTheme === THEME.dark ? THEME.dark : THEME.light);
+txtBtnTheme.innerText = `Tema ${savedTheme === THEME.dark ? STR_THEME.light : STR_THEME.dark}`;
+
 
 if (btnTheme) {
   btnTheme.addEventListener('click', toggleTheme);
