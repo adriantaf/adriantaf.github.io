8f996620f0659babb670c2e47938601f9015a547
adriantaf <atafoya863@gmail.com>
2026-04-22 10:57:21 -0700

    Textos, links e imagenes finales

---
+++ src/config/social_medias.ts
@@ -0,0 +1,18 @@
+export const SM = {
+  whatsapp: {
+    link: "https://wa.me/526461363812",
+    label: "WhatsApp",
+  },
+  email: {
+    link: "https://mailto:atafoya863@gmail.com",
+    label: "Email",
+  },
+  // instagram: {
+  //   link: "https://instagram.com/tafoya",
+  //   label: "Instagram",
+  // },
+  // facebook: {
+  //   link: "https://facebook.com/tuusuario",
+  //   label: "Facebook",
+  // },
+};
